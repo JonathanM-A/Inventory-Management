0fd15e11ab49929ada406ec2118c703b5f894784
JonathanM-A <kamajthomas@gmail.com>
2024-09-28 10:34:05 +0000

    Initial commit

---
+++ inventory_management/app/app.py
@@ -0,0 +1,36 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+from flask import Flask
+from flask_migrate import Migrate
+import os
+from datetime import timedelta
+
+
+db = SQLAlchemy()
+jwt = JWTManager()
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"postgresql://{os.getenv('user')}:{os.getenv('password')}@{os.getenv('host')}:{os.getenv('port')}/{os.getenv('database')}"
+    )
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config["UPLOAD_FOLDER"] = "uploads"
+    app.config["SECRET_KEY"] = os.getenv("secret_key")
+    app.config["JWT_SECRET_KEY"] = os.getenv("secret_key")
+    app.config["JWT_TOKEN_LOCATION"] = ["headers"]
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=24)
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    from routes import register_routes
+    register_routes(app, db)
+
+    migrate = Migrate(app, db)
+
+    return (app)
+
